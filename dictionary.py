324d5b1cab14a8b79e78b22bb54104df06327155
Shubham Naik <76036946+shubhamnaikk@users.noreply.github.com>
2021-08-14 21:40:53 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+dic1={
+    "junkfood":"unhealthy",
+    "vegan":"non-dairy products",
+    "sweets":"sugary food",
+    "pen":"used to write"}
+
+
+print("Please enter the word whose meaning you want")
+a=input()
+print(dic1[a])
